b95a4870a5113e88b393d9a52f94331cbd0a2e4a
Avdey <avdeev.1van@icloud.com>
2019-05-10 23:00:32 +0300

    add node server

---
+++ backend/src/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const config = require('./config/index')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+)
+app.use(cors())
+
+app.use(morgan('dev'))
+
+app.listen(config.port, () => {
+  console.log(`Listening on port ${config.port}`)
+})
+
+app.get('/', (req, res) => {
+  res.send('Simple backend response').status(200)
+})
