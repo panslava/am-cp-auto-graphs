fbd395e2b5794383fffb948aa7015894e04246a9
Avdey <avdeev.1van@icloud.com>
2019-05-10 22:37:43 +0300

    add first algos

--- frontend/am-cp-graph-frontend/src/app/app-routing.module.ts
+++ frontend/am-cp-graph-frontend/src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ListOfTasksComponent} from './list-of-tasks/list-of-tasks.component';
+import {StrongConnectedComponent} from './strong-connected/strong-connected.component';
+import {MatchingsComponent} from './matchings/matchings.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ListOfTasksComponent,
     pathMatch: 'full'
+  },
+  {
+    path: 'strong_connected',
+    component: StrongConnectedComponent
+  },
+  {
+    path: 'matchings',
+    component: MatchingsComponent
   }
 ];
 
---
+++ frontend/am-cp-graph-frontend/src/app/matchings/matchings.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {SendDataService} from '../send-data.service';
+
+@Component({
+  selector: 'app-matchings',
+  templateUrl: './matchings.component.html',
+  styleUrls: ['./matchings.component.css']
+})
+export class MatchingsComponent implements OnInit {
+
+  private task: FormGroup;
+  isLoad = false;
+  res;
+
+  constructor(private fb: FormBuilder, private sendDataService: SendDataService) { }
+
+  ngOnInit() {
+    this.task = this.fb.group({
+      task: ['', [Validators.required]]
+    });
+
+  }
+
+  sendData() {
+    this.sendDataService.strongConnected(this.task.value.task).subscribe(res => {
+      this.isLoad = true;
+      this.res = res;
+    });
+  }
+
+}
